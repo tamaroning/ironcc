ad93bfd7fa6545f58a5abde7f04b9567cad7ecbb
tamaroning <20992019+tamaroning@users.noreply.github.com>
2021-09-19 00:17:52 +0900

    add version shower

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod version;
+
 fn main() {
-    println!("Hello, Rust Kaleidoscope!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        version::show_version();
+    } else {
+        println!("Hello, Rust Kaleidoscope!");
+    }
 }
---
+++ src/version.rs
@@ -0,0 +1,5 @@
+const VERSION_STR: &'static str = "0.1.0";
+
+pub fn show_version() {
+    println!("rust-kaleidocope version {}", VERSION_STR);
+}
