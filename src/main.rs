075d4f66a4dd3ec60eaa0be1bc18926ec17dc484
tamaroning <20992019+tamaroning@users.noreply.github.com>
2021-09-19 00:52:21 +0900

    make it possible to open files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod version;
 
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         version::show_version();
     } else {
-        println!("Hello, Rust Kaleidoscope!");
+        let input_file_name = &args[1];
+        let mut input_file = File::open(input_file_name).expect("File not found");
+
+        let mut string = String::new();
+        input_file.read_to_string(&mut string).expect("Couldn't open the file");
+
+        println!("content: {}", string);
+
     }
 }
