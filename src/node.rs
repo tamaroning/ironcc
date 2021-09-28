cf4250caabe43a6fa5b97d9029f822617c870788
tamaroning <20992019+tamaroning@users.noreply.github.com>
2021-09-28 22:06:57 +0900

    prepare macro

--- src/node.rs
+++ src/node.rs
@@ -16,3 +16,28 @@ pub enum BinaryOp {
     Lt, // <
     Le, // <=
 }
+
+impl AST {
+    pub fn eval_const_expr(&self) -> i32 {
+        match &self {
+            AST::Int(n) => *n,
+            AST::BinaryOp(l, r, op) => {
+                let l = l.eval_const_expr();
+                let r = r.eval_const_expr();
+                match op {
+                    &BinaryOp::Add => l + r,
+                    &BinaryOp::Sub => l - r,
+                    &BinaryOp::Mul => l * r,
+                    &BinaryOp::Div => l / r,
+                    &BinaryOp::Eq => (l == r) as i32,
+                    &BinaryOp::Ne => (l != r) as i32,
+                    &BinaryOp::Lt => (l < r) as i32,
+                    &BinaryOp::Le => (l <= r) as i32,
+                    _ => panic!("Unknown operator"),
+                }
+
+            },
+            _ => panic!("Expected constant expression"),
+        }
+    }
+}
